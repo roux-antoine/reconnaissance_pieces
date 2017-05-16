efdc31c276e69e82824fe6f336eb48104ba8f845
Christophe BOETTO <christophe.boetto@ensta-paristech.fr>
2017-05-16 14:23:35 +0200

    calcul de l'angle du gradient et de son poid ET algorithme de contour affiné

--- contour.py
+++ contour.py
@@ -15,19 +15,33 @@ img = cv2.imread('pièce.jpeg', 0) #image noir et blanc de la pièce
 length = len(img[1])
 height = len(img)
 
-seuil_bas = 0
-seuil_haut = 50
+seuil_bas = 150
+seuil_haut = 350
 
 mat_cont = np.zeros((height,length))
 
 for i in range (1, height):
     for j in range (1, length):
-        if ((grd.grad_hor(img,i,j)<seuil_bas) or (grd.grad_vert(img,i,j)<seuil_bas)):
+        if (grd.force_grad(img,i,j)<seuil_bas):
             mat_cont [i,j] = 255
-        elif ((grd.grad_hor(img,i,j)>seuil_haut) or (grd.grad_vert(img,i,j)>seuil_haut)):
+        elif (grd.force_grad(img,i,j)>seuil_haut):
             mat_cont [i,j] = 0
         else :
             mat_cont [i,j] = 150
+
+#still_grey = 1
+#while(still_grey==1):
+#    still_grey = 0
+#    for i in range (1,height):
+#        for j in range (1,length):
+#            if (mat_cont[i+1,j]==0 or mat_cont[i-1,j] == 0 or mat_cont[i,j+1]==0 or mat_cont[i,j-1]==0):
+#                mat_cont[i,j] = 0
+#            elif (mat_cont[i+1,j]==150 or mat_cont[i-1,j] == 150 or mat_cont[i,j+1]==150 or mat_cont[i,j-1]==150):
+#                still_grey = 1;
+#            else :
+#                mat_cont[i,j]=255
+#    cv2.imshow('pièce', mat_cont)
+#    cv2.waitKey(0)
 
 cv2.imshow('pièce', mat_cont)
 cv2.waitKey(0) #on attend que l'utilisateur appuye sur une touche pour agir
--- gradient.py
+++ gradient.py
@@ -5,8 +5,17 @@ Created on Tue May  9 15:49:14 2017
 @author: boetto
 """
 
+import numpy as np
+
 def grad_hor(img,x,y):
     return img[x][y]-img[x-1][y]
 
 def grad_vert(img,x,y):
     return img[x][y]-img[x][y-1]
+
+def angle_grad(img, x,y):
+    return np.atan(grad_vert(img,x,y)/grad_hor(img,x,y))
+
+def force_grad(img,x,y):
+    return np.sqrt(grad_vert(img,x,y)**2 + grad_hor(img,x,y)**2)
+
