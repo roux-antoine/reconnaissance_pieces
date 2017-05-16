44f08d4556da10969e5d1a621904afc6ec0e038e
Antoine Roux <antoine.roux@ensta-paristech.fr>
2017-05-16 14:17:21 +0200

    amelioration floutage

--- floutage.py
+++ floutage.py
@@ -5,13 +5,16 @@ Created on Tue May 16 13:41:14 2017
 @author: aroux
 """
 
-#script/fonction qui prend en a arg une image et la renvoit floutee
+#script qui prend en arg une image et la renvoie floutee
 
 import cv2
 
 img = cv2.imread('pièce.jpeg',0) #image noir et blanc de la pièce
 
-img_blurred = cv2.GaussianBlur(img,(5,5), 0)
+kernel_size = 7
+#ATTENTION : kernel_size doit etre un nbr impair 
+
+img_blurred = cv2.GaussianBlur(img,(kernel_size,kernel_size), 0)
 
 cv2.imshow('img_blurred', img_blurred)
 cv2.waitKey(0) #on attend que l'utilisateur appuye sur une touche pour agir
