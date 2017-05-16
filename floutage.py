b8a96206dc98db732401342eb0cdd7c46a67071d
Antoine Roux <antoine.roux@ensta-paristech.fr>
2017-05-16 14:27:34 +0200

    fait de foutage.py une fonction et non plus un script

--- floutage.py
+++ floutage.py
@@ -5,16 +5,20 @@ Created on Tue May 16 13:41:14 2017
 @author: aroux
 """
 
-#script qui prend en arg une image et la renvoie floutee
+
 
 import cv2
 
-img = cv2.imread('pièce.jpeg',0) #image noir et blanc de la pièce
+def blur_image(img) : 
+    """fonction qui prend en arg une image et la renvoie floutee"""
 
-kernel_size = 7
-#ATTENTION : kernel_size doit etre un nbr impair 
+    kernel_size = 7
+    #ATTENTION : kernel_size doit etre un nbr impair 
+    img_blurred = cv2.GaussianBlur(img,(kernel_size,kernel_size), 0)
+    
+    return img_blurred
 
-img_blurred = cv2.GaussianBlur(img,(kernel_size,kernel_size), 0)
-
-cv2.imshow('img_blurred', img_blurred)
-cv2.waitKey(0) #on attend que l'utilisateur appuye sur une touche pour agir
+    #cv2.imshow('img_blurred', img_blurred)
+    #cv2.waitKey(0) #on attend que l'utilisateur appuye sur une touche pour agir
+    
+    
