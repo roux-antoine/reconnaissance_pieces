7d28172a5296d51dcc15654c0186cf12b76d2a3f
Antoine Roux <antoine.roux@ensta-paristech.fr>
2017-05-30 17:01:00 +0200

    modifie circle_follower, en cours de resolution

--- circle_follower_2.py
+++ circle_follower_2.py
@@ -22,7 +22,7 @@ def new_position_finder (old_img, new_img, x_center, y_center, radius) :
     print(size_image_x)
     print(size_image_y)
     
-#    cv2.circle(old_img, (x_center, y_center), 25, (0,0,255))
+#    cv2.circle(old_img, (x_center, y_center), radius, (0,0,255))
 #    cv2.imshow('pièce', old_img)
 #    cv2.waitKey(0) #on attend que l'utilisateur appuye sur une touche pour agir
 #    cv2.destroyAllWindows() #on ferme tout
@@ -38,22 +38,22 @@ def new_position_finder (old_img, new_img, x_center, y_center, radius) :
     print(old_circle_square)
     print("\n")
     
-    matrice_distances = np.zeros([size_image_x, size_image_y])
+    matrice_distances = np.zeros([size_image_x-2*radius, size_image_y-2*radius])
         
-    for i in range (0, size_image_x) : 
-        print("Progression =", (i+1), "sur", (size_image_x))
+    for i in range (0, size_image_x-2*radius) : 
+        print("Progression =", (i+1), "sur", (size_image_x-2*radius))
         print("\n")
-        for j in range (0, size_image_y):
+        for j in range (0, size_image_y-2*radius):
             
-            square_in_new = np.zeros([2*radius+1, 2*radius+1])
+            square_in_new = new_img[i:i+2*radius+1, j:j+2*radius+1]
             
-            for k in range (0, 2*radius) :
-                for l in range (0, 2*radius) :
-        
-                    square_in_new[k,l] = new_img[i+k, j+l]                 
-                    
-                    erreur_entre_pixels = (old_circle_square[k,l]-square_in_new[k,l])**2
-                    matrice_distances[i,j] += erreur_entre_pixels
+            matrice_distances[i,j] = np.linalg.norm(square_in_new - old_circle_square)
+            
+#            for k in range (0, 2*radius) :
+#                for l in range (0, 2*radius) :             
+#                    
+#                    erreur_entre_pixels = (old_circle_square[k,l]-square_in_new[k,l])**2
+#                    matrice_distances[i,j] += erreur_entre_pixels
                     
          
     print("matrice des distances")
@@ -61,14 +61,15 @@ def new_position_finder (old_img, new_img, x_center, y_center, radius) :
     #la position du 0 dans matcrice_distances nous donne le deplacement du motif
     
     #il faut maintenant trouver sa position
-    indice_min = np.argmin(matrice_distances) #il correspond à la postion en mode liste
-    deplacement_x = (indice_min) % (size_image_y)
-    deplacement_y = (indice_min) // (size_image_y)
-    print('deplacement_x :',deplacement_x, 'deplacement_y :', deplacement_y)
+    """ A CHANGER """
+    indice_min = np.argmin(matrice_distances)#il correspond à la postion en mode liste
+    new_pos_x = (indice_min) % (size_image_y)
+    new_pos_y = (indice_min) // (size_image_y)
+    print('new_pos_x :',new_pos_x, 'new_pos_y :', new_pos_y)
     
     
-#               
-#    cv2.circle(new_img, (deplacement_x, deplacement_y), 25, (0,0,255))  
+               
+#    cv2.circle(new_img, (new_pos_x, new_pos_y), 25, (0,0,255))  
 #    cv2.imshow('pièce', new_img)
 #    cv2.waitKey(0) #on attend que l'utilisateur appuye sur une touche pour agir
 #    cv2.destroyAllWindows() #on ferme tout
@@ -76,7 +77,7 @@ def new_position_finder (old_img, new_img, x_center, y_center, radius) :
 
 
 
-test = 1
+test = 2
 
 if test == 1 : 
 
@@ -108,8 +109,8 @@ if test == 2 :
 
 if test == 3 : 
     
-    img1 = np.array([[1,2,3,0,0],[4,5,6,0,0],[7,8,9,0,0],[0,0,0,0,0],[0,0,0,0,0]])
-    img2 = np.array([[0,0,0,0,0],[0,0,1,2,3],[0,0,4,5,6],[0,0,7,8,9],[0,0,0,0,0]])
+    img2 = np.array([[1,2,3,0,0],[4,5,6,0,0],[7,8,9,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+    img1 = np.array([[0,0,0,0,0],[0,0,1,2,3],[0,0,4,5,6],[0,0,7,8,9],[0,0,0,0,0]])
     
     print("ancienne image")
     print(img1)
@@ -118,13 +119,13 @@ if test == 3 :
     print(img2)
     print("\n")
         
-    new_position_finder(img1, img2, 1, 1, 1)
+    new_position_finder(img1, img2, 3, 2, 1)
 
 if test == 4 : 
     img1 = cv2.imread('photo1new.JPG', 0) #image noir et blanc de la pièce
     img2 = cv2.imread('photo2new.JPG', 0) #image noir et blanc de la pièce
     
-    img1 = imres.image_resizer(img1, 0.01)
-    img2 = imres.image_resizer(img2, 0.01)
+    img1 = imres.image_resizer(img1, 0.1)
+    img2 = imres.image_resizer(img2, 0.1)
     
-    new_position_finder(img1, img2, 85, 165, 3)
+    new_position_finder(img1, img2, 85, 165, 50)
